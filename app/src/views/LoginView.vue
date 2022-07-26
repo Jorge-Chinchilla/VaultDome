a7f5b307ed27daafabc772a18a51b6450fc9680a
Antony Brenes <antonybrenescruz@gmail.com>
2022-07-25 21:23:05 -0600

    Iniciando frontend de la app

---
+++ app/src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import HelloWorldVue from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+  <main>
+    <h1>LOGIN</h1>
+    <HelloWorldVue msg="Industria" />
+  </main>
+</template>
