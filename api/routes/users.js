028594b9533390a2998d150abecf5d01602a89d1
Antony Brenes <antonybrenescruz@gmail.com>
2022-08-17 00:47:06 -0600

    Actualizando API para no recibir si el usuario es admin por la request

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,160 +1,160 @@
 const User = require('../models/User');
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
-const {createAccessToken, checkSession, getUserData} = require('../controllers/auth.controllers')
+const { createAccessToken, checkSession, getUserData } = require('../controllers/auth.controllers')
 
 //Manejo de usuarios
 router.route('/:id')
     //Actualizar un usuario
-    .put(checkSession, async(req, res)=>{
+    .put(checkSession, async (req, res) => {
 
         let userData = getUserData(req.headers.authorization);
 
-        if(userData._id === req.params.id || userData.isAdmin){
-            if(req.body.password){
+        if (userData._id === req.params.id || userData.isAdmin) {
+            if (req.body.password) {
                 try {
                     const salt = await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS));
                     req.body.password = await bcrypt.hash(req.body.password, salt);
-                }catch (e) {
-                    return res.status(500).json({"message": e});
+                } catch (e) {
+                    return res.status(500).json({ "message": e });
                 }
             }
             try {
                 const user = await User.findByIdAndUpdate(req.params.id, {
-                    $set:req.body
+                    $set: req.body
                 });
-                res.status(200).json({accessToken: createAccessToken(user)});
+                res.status(200).json({ accessToken: createAccessToken(user) });
             } catch (e) {
-                return res.status(500).json({"message": e});
+                return res.status(500).json({ "message": e });
             }
-        }else{
-            return res.status(403).json({"message": "You can only update your account"});
+        } else {
+            return res.status(403).json({ "message": "You can only update your account" });
         }
     })
     //Eliminar un usuario
-    .delete(checkSession, async(req, res)=>{
+    .delete(checkSession, async (req, res) => {
 
         let userData = getUserData(req.headers.authorization);
 
-        if(userData._id === req.params.id || req.body.isAdmin){          
+        if (userData._id === req.params.id || userData.isAdmin) {
 
             try {
                 await User.findByIdAndDelete(req.params.id);
-                res.status(200).json({"message": "Account deleted"});
+                res.status(200).json({ "message": "Account deleted" });
             } catch (e) {
-                return res.status(500).json({"message": e});
+                return res.status(500).json({ "message": e });
             }
-        }else{
-            return res.status(403).json({"message": "You can only delete your account"});
+        } else {
+            return res.status(403).json({ "message": "You can only delete your account" });
         }
     });
 
 //Obtener un usuario por nombre o por id
-router.get("/", checkSession, async (req,res)=>{
+router.get("/", checkSession, async (req, res) => {
 
     const userId = req.query.userId;
     const username = req.query.username;
-    try{
+    try {
         let user = userId
             ? await User.findById(userId)
-            : await User.findOne({username: username});
-        
+            : await User.findOne({ username: username });
 
-        let followers = await Promise.all(user.followers.map(async (userId)=>{
+
+        let followers = await Promise.all(user.followers.map(async (userId) => {
             let user = await User.findById(userId)
-            return {id: user.id, username: user.username}
+            return { id: user.id, username: user.username }
         }))
 
-        
 
-        
-        let following = await Promise.all(user.following.map(async (userId)=>{
+
+
+        let following = await Promise.all(user.following.map(async (userId) => {
             let user = await User.findById(userId)
-            return {id: user.id, username: user.username}
+            return { id: user.id, username: user.username }
         }))
 
 
         user.followers = followers
         user.following = following
 
-        
-        
+
+
 
         //destructuramos en un array el objeto, guardando la información que deseamos imprimir en "others"
-        const {password, updatedAt, createdAt, __v, ...other} = user._doc;
+        const { password, updatedAt, createdAt, __v, ...other } = user._doc;
         res.status(200).json(other);
     } catch (e) {
-        res.status(500).json({"message": e});
+        res.status(500).json({ "message": e });
     }
 });
 
 
 //Seguir a un usuario
 router.route('/:id/follow')
-    .put(checkSession, async(req,res)=>{
+    .put(checkSession, async (req, res) => {
 
         let userData = getUserData(req.headers.authorization);
 
-        if(userData._id !== req.params.id){
+        if (userData._id !== req.params.id) {
             try {
                 const user = await User.findById(req.params.id);
                 const currentUser = await User.findById(userData._id);
-                if(!currentUser.following.includes(req.params.id)){
+                if (!currentUser.following.includes(req.params.id)) {
                     //si el usuario no existe en la lista de seguidores
-                    await user.updateOne({$push: {followers: userData._id}});
-                    await currentUser.updateOne({$push: {following: req.params.id}});
+                    await user.updateOne({ $push: { followers: userData._id } });
+                    await currentUser.updateOne({ $push: { following: req.params.id } });
                     let UpdatedCurrentUser = await User.findById(userData._id);
-                    res.status(200).json({accessToken: createAccessToken(UpdatedCurrentUser)});
-                }else{
+                    res.status(200).json({ accessToken: createAccessToken(UpdatedCurrentUser) });
+                } else {
                     //si el usuario ya existe en la lista de seguidores
-                    res.status(403).json({"message": "You already follow this user"});
+                    res.status(403).json({ "message": "You already follow this user" });
                 }
-            }catch (e) {
-                res.status(500).json({"message": e});
+            } catch (e) {
+                res.status(500).json({ "message": e });
             }
-        }else{
-            res.status(403).json({"message": "You can't follow yourself"});
+        } else {
+            res.status(403).json({ "message": "You can't follow yourself" });
         }
     });
 //Dejar de seguir a un usuario
 router.route('/:id/unfollow')
-    .put(checkSession, async(req,res)=>{
+    .put(checkSession, async (req, res) => {
 
         let userData = getUserData(req.headers.authorization);
-       
-        if(userData._id !== req.params.id){
+
+        if (userData._id !== req.params.id) {
             try {
                 const user = await User.findById(req.params.id);
                 let currentUser = await User.findById(userData._id);
-                if(user.followers.includes(userData._id)){
+                if (user.followers.includes(userData._id)) {
                     //si el usuario no existe en la lista de seguidores
-                    await user.updateOne({$pull: {followers: userData._id}});
-                    await currentUser.updateOne({$pull: {following: req.params.id}});
+                    await user.updateOne({ $pull: { followers: userData._id } });
+                    await currentUser.updateOne({ $pull: { following: req.params.id } });
                     currentUser = await User.findById(userData._id);
-                    res.status(200).json({accessToken: createAccessToken(currentUser)});
-                }else{
+                    res.status(200).json({ accessToken: createAccessToken(currentUser) });
+                } else {
                     //si el usuario ya existe en la lista de seguidores
                     res.status(403).json("You don't follow this user");
                 }
-            }catch (e) {
-                res.status(500).json({"message": e});
+            } catch (e) {
+                res.status(500).json({ "message": e });
             }
-        }else{
-            res.status(403).json({"message": "You can't unfollow yourself"});
+        } else {
+            res.status(403).json({ "message": "You can't unfollow yourself" });
         }
     });
 
 router.route('/all')
-.get(checkSession, async (req, res) =>{
-    
-    try {
-        const users = await User.find({});
-        return res.status(200).json(users);
-    } catch (err) {
-        res.status(500).json({ "message": err });
-        console.log(err)
-    }
-})
+    .get(checkSession, async (req, res) => {
+
+        try {
+            const users = await User.find({});
+            return res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json({ "message": err });
+            console.log(err)
+        }
+    })
 
 
 
